14bd27b50b8cb6be794747b7e1eeb9864f5e19e4
javierpb275 <javierpb275@gmail.com>
2021-06-26 17:36:56 +0200

    using request package and weatherstack api to access the current forecast in New York

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const request = require('request');//make http calls. It supports HTTPS and follows redirects by default.
 
+const url = 'http://api.weatherstack.com/current?access_key=0e93adc7da91435c4890075e60c5c6b1&query=New%20York';
 
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+})
