81400a402ef6d13c7f4605ba9079c9e81381749a
javierpb275 <javierpb275@gmail.com>
2021-06-27 13:23:03 +0200

    passed json:true in request so we do not have to use JSON.parse() since the data comes already parsed. Accessing the temperature and the chance of precipitation and printing that data.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require('request');//make http calls. It supports HTTPS and foll
 
 const url = 'http://api.weatherstack.com/current?access_key=0e93adc7da91435c4890075e60c5c6b1&query=New%20York';
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({url: url, json: true}, (error, response) => {
+    console.log(`It is ${response.body.current.temperature} degrees out.`);
+    console.log(`There is a ${response.body.current.precip}% chance of rain.`);
 })
