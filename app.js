f25e1dd54dad7bb52c71643fc0d30af14fd9a841
javierpb275 <javierpb275@gmail.com>
2021-06-29 19:07:06 +0200

    handling errors with if and else

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const request = require('request');//make http calls. It supports HTTPS and follows redirects by default.
 
-const urlWeatherstack = 'http://api.weatherstack.com/current?access_key=0e93adc7da91435c4890075e60c5c6b1&query=New%20York';
+const urlWeatherstack = 'http://api.weatherstack.com/current?access_key=5287c1ed8a65a3139af80d11998a60ca&query=New%20York';
 
 request({url: urlWeatherstack, json: true}, (error, response) => {
-    console.log(`It is ${response.body.current.temperature} degrees out.`);
-    console.log(`There is a ${response.body.current.precip}% chance of rain.`);
+    if(error) {
+        console.log('Unable to connect to weather service!');
+    } else if(response.body.error) {
+        console.log('Unable to find location');
+    } else {
+        console.log(`It is ${response.body.current.temperature} degrees out.`);
+        console.log(`There is a ${response.body.current.precip}% chance of rain.`);
+    }
 })
 
 const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamF2aWVycGIyNzUiLCJhIjoiY2txaTg4ZTRzMXdpYzJ1bWltbXUwZXNhZiJ9.dVUptpdm3nMBcwc95GwIXg&limit=1';
 
 request({url: urlMapbox, json: true}, (error, response) => {
-    const longitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1];
-    console.log(`latitude: ${latitude}`);
-    console.log(`longitude: ${longitude}`);
+    if(error) {
+        console.log('Unable to connect to location service!');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location!');
+    } else {
+        const longitude = response.body.features[0].center[0];
+        const latitude = response.body.features[0].center[1];
+        console.log(`latitude: ${latitude}`);
+        console.log(`longitude: ${longitude}`);
+    }
 })
